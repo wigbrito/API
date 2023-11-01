77cead578af1393e366bb5bdb7247c2255b4299f
William Goncalves Brito - PrestServ <wigbrito@gmail.com>
2023-10-31 21:05:48 -0300

    first commit api project

---
+++ src/main/java/br/com/projeto/api/Controller.java
@@ -0,0 +1,34 @@
+package br.com.projeto.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.model.Pessoa;
+
+@RestController
+public class Controller {
+
+    @GetMapping("")
+    public String mensagem() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Seja bem vindo(a)! ";
+    }
+
+    @GetMapping("/boasVindas/{nome}")
+    public String boasVindas(@PathVariable String nome) {
+        return "Seja bem vindo(a)! " + nome;
+    }
+
+    @PostMapping("/pessoa")
+    public Pessoa pessoa(@RequestBody Pessoa p) {
+        return p;
+    }
+
+}
---
+++ src/main/java/br/com/projeto/api/model/Pessoa.java
@@ -0,0 +1,45 @@
+package br.com.projeto.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pessoas")
+public class Pessoa {
+
+    // atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigo;
+    private String nome;
+    private int idade;
+
+    // geters and setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+}
