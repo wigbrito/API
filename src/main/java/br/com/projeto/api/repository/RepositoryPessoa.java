8dabe4bef72ccba3579124ecfa1b0167a9566147
William Goncalves Brito - PrestServ <wigbrito@gmail.com>
2023-10-31 23:12:08 -0300

    repository

---
+++ src/main/java/br/com/projeto/api/repository/RepositoryPessoa.java
@@ -0,0 +1,11 @@
+package br.com.projeto.api.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.projeto.api.model.Pessoa;
+
+@Repository
+public interface RepositoryPessoa extends CrudRepository<Pessoa, Integer> {
+
+}
