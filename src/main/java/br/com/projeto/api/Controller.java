1b9ace6969fbffbf7b99571e9c8b254024fe37d1
William Goncalves Brito - PrestServ <wigbrito@gmail.com>
2023-11-01 00:13:52 -0300

    update

--- src/main/java/br/com/projeto/api/Controller.java
+++ src/main/java/br/com/projeto/api/Controller.java
@@ -1,5 +1,6 @@
 package br.com.projeto.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +8,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.api.model.Pessoa;
+import br.com.projeto.api.repository.RepositoryPessoa;
 
 @RestController
 public class Controller {
+
+    @Autowired
+    private RepositoryPessoa acao;
+
+    @PostMapping("/api")
+    public Pessoa cadastrar(@RequestBody Pessoa obj) {
+        return acao.save(obj);
+    }
 
     @GetMapping("")
     public String mensagem() {
