a384edf927f3b26695ecfe35496b773dd033870f
wigbrito <wigbrito@gmail.com>
2023-11-12 19:46:18 -0300

    COMMIT AUTOMATIZADO

---
+++ src/main/java/br/com/projeto/api/model/Cliente.java
@@ -0,0 +1,41 @@
+package br.com.projeto.api.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Clientes")
+public class Cliente {
+    
+    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigo;
+    private String nome;
+    private String email;
+
+
+    //Get e Set
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
